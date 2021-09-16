164392047927f326448af39964444805f8c6a296
Bren Saud <brensaud4@gmail.com>
2021-09-16 16:09:32 +0545

    Delete Implemented

--- formscrud/urls.py
+++ formscrud/urls.py
@@ -8,6 +8,7 @@ from .views import (
 )
 urlpatterns = [
     path('', FlightListView.as_view(), name = 'home'),
+    path('flight/<int:pk>/delete', FlightDeleteView.as_view(), name='delete_flight'),
     path('flight/<int:pk>/', FlightDetailView.as_view(), name = 'flight_detail'),
     path('flight/<int:pk>/edit/', FlightUpdateView.as_view(), name = 'flight_edit'),
     path('flight/new/', FlightCreateView.as_view(), name = 'create_flight'),
